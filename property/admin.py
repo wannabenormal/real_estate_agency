ead34f6f507d8d493f5ccc32b17f8541d8e22236
Alexey Pesotsky <pesthedog@mail.ru>
2022-02-15 18:59:31 +0400

    Setup admin UI for likes

--- property/admin.py
+++ property/admin.py
@@ -11,6 +11,7 @@ class FlatAdmin(admin.ModelAdmin):
                     'town')
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony', 'active')
+    raw_id_fields = ('liked_by',)
 
 
 @admin.register(Report)
