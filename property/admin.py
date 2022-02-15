3c6111d260c84381bdd2b4e717539e5a5249e04e
Alexey Pesotsky <pesthedog@mail.ru>
2022-02-15 18:53:25 +0400

    Add db model for reports

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Report
 
 
 @admin.register(Flat)
@@ -11,3 +11,8 @@ class FlatAdmin(admin.ModelAdmin):
                     'town')
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony', 'active')
+
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user', 'flat')
--- property/models.py
+++ property/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
@@ -50,3 +51,17 @@ class Flat(models.Model):
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+class Report(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    flat = models.ForeignKey(
+        Flat,
+        on_delete=models.CASCADE,
+        verbose_name='Квартира'
+    )
+    message = models.TextField('Текст жалобы')
