632beee9bcb64a38cbf74a557892db8e0a3fac1e
Alexey Pesotsky <pesthedog@mail.ru>
2022-02-16 21:06:39 +0400

    Remove deprecated owners fields from Flat

--- property/admin.py
+++ property/admin.py
@@ -5,10 +5,10 @@ from .models import Flat, Report, Owner
 
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('owner', 'town', 'address')
+    search_fields = ('town', 'address')
     readonly_fields = ('created_at',)
     list_display = ('address', 'price', 'new_building', 'construction_year',
-                    'town', 'owners_phonenumber', 'owner_pure_phone')
+                    'town')
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony', 'active')
     raw_id_fields = ('liked_by',)
--- property/models.py
+++ property/models.py
@@ -5,12 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owner_pure_phone = PhoneNumberField(
-        'Нормализованный номер владельца',
-        blank=True
-    )
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     new_building = models.BooleanField('Новостройка', null=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
